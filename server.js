a54516a12786e113aab2ebced83c120e88bd025c
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-05-12 13:59:24 +1000

    Step 9: Parse posts.json :building_construction:

--- server.js
+++ server.js
@@ -14,20 +14,31 @@ app.get('/get-posts', function(request, response) {
       response.status(500)
       response.send(error)
     } else {
-      response.send(data.toString())
+      response.send(data)
     }
   })
 })
 
 app.post('/create-post', function(request, response) {
   var now = Date.now()
-
   var newPost = {
     timestamp: now,
     content: request.fields.blogpost
   }
 
-  response.send(newPost)
+  fs.readFile(__dirname + '/data/posts.json', function(error, data) {
+    if (error) {
+      console.log('Error reading posts.json: ' + error)
+      response.status(500)
+      response.send(error)
+    } else {
+      var posts = JSON.parse(data)
+      posts.blogposts.push(newPost)
+      console.log(posts.blogposts)
+
+      response.send(newPost)
+    }
+  })
 })
 
 app.listen(8080, function() {
