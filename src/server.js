409ceedb5e74bed5d864cfbe148a4fa16bfca555
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-05-12 11:41:08 +1000

    Step 5: Serve static assets; remove routes :building_construction:

--- src/server.js
+++ src/server.js
@@ -2,13 +2,7 @@ var express = require('express')
 
 var app = express()
 
-app.get('/hello', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.get('/chocolate', function(request, response) {
-  response.send('Mmmm, chocolate...')
-})
+app.use(express.static('public'))
 
 app.listen(8080, function() {
   console.log('Server has started listening on port 8080.')
