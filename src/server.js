133f268c5bd1b876feac664912a64e39290bb432
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-05-12 11:24:20 +1000

    Step 4: Add endpoints :building_construction:

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ var express = require('express')
 
 var app = express()
 
+app.get('/hello', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.get('/chocolate', function(request, response) {
+  response.send('Mmmm, chocolate...')
+})
+
 app.listen(8080, function() {
   console.log('Server has started listening on port 8080.')
 })
